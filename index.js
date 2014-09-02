3652fb70bd00c53fa3306e5607e232f34b7ae25f
Max Galbusera <max.galbusera@gmail.com>
2014-09-02 11:41:31 +0200

    remove features already available in gulp-include

--- index.js
+++ index.js
@@ -7,35 +7,9 @@ var PluginError = gulputil.PluginError;
 
 const PLUGIN_NAME = 'gulp-ractive';
 
-function escapeRegExp(str)
-{
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-}
-
 function gulpRactive(options)
 {
 	var delimiters = options.delimiters || ['{{','}}'];
-	
-	//Regexp to make includes work in ractive file
-	//String to match (with delimiters (by default {{ and }}) changeable through options
-	//{{ include("file.ractive") }}
-	var includeRegexp = new RegExp(escapeRegExp(delimiters[0]) +
-		'\\s*include\\s*\\(\\s*"{1}([^"]+)"{1}\\s*\\)\\s*' + escapeRegExp(delimiters[1]), "gi");
-	var includeReplaceFunction = function(currentfile) {
-		return function(match, includedfilename) {
-			var filepath = currentfile.base +"/"+ includedfilename;
-			
-			if (!fs.existsSync(filepath))
-				callback(new PluginError(PLUGIN_NAME, 'Included file not found: ' + includedfilename));
-			else
-			{
-				gulputil.log('gulp-ractive:', chalk.green('included file: ') + includedfilename);
-				return fs.readFileSync(filepath);
-			}
-			
-			return "";
-		};
-	};
 	
 	var stream = through.obj(function(file, enc, callback) {
 		if (file.isStream())
@@ -44,17 +18,15 @@ function gulpRactive(options)
 			return callback();
 		}
 		
-		var compiledtemplate = null, filecontents = "";
+		var filecontents = "";
 		
 		try {
-			//Find include calls
 			filecontents = String(file.contents);
-			filecontents = filecontents.replace(includeRegexp, includeReplaceFunction(file, callback));
 			
 			//Parse template in Ractive
-			compiledtemplate = Ractive.parse(filecontents, options);
-			compiledtemplate = JSON.stringify(compiledtemplate);
-			file.contents = new Buffer(compiledtemplate);
+			filecontents = Ractive.parse(filecontents, options);
+			filecontents = JSON.stringify(filecontents);
+			file.contents = new Buffer(filecontents);
 			this.push(file);
 		}
 		catch (e) {
